4b5c387b5f350525dcc598582cdb66997c26c3da
tien n <tien.n@sbcglobal.net>
2016-03-14 10:55:57 -0700

    Changes in java class 'App.java' for testing the changes

--- NumberGenerator/NumberGenerator/src/main/java/com/loti/App.java
+++ NumberGenerator/NumberGenerator/src/main/java/com/loti/App.java
@@ -12,7 +12,8 @@ public class App
 	 public static void main( String[] args )
 	    {
 	        App obj = new App();
-	        System.out.println("UniqueID : " + obj.generateUniqueKey());
+	        System.out.println("Unique ID : " + obj.generateUniqueKey());
+	        System.out.println("change on local branch 03/14/2016");
 	    }
 	    
 	    public String generateUniqueKey(){
